18c370e31f23a4a1b8476c363791eb1f5eb02f11
simon <brotherlogic@gmail.com>
2021-08-04 13:08:48 -0700

    Adds in CLI

---
+++ recordfanout_cli/cli.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/brotherlogic/goserver/utils"
+
+	pb "github.com/brotherlogic/recordfanout/proto"
+
+	//Needed to pull in gzip encoding init
+	_ "google.golang.org/grpc/encoding/gzip"
+	"google.golang.org/grpc/resolver"
+)
+
+func init() {
+	resolver.Register(&utils.DiscoveryClientResolverBuilder{})
+}
+
+func main() {
+	ctx, cancel := utils.ManualContext("recordfanout-cli", time.Second*10)
+	defer cancel()
+
+	conn, err := utils.LFDialServer(ctx, "recordfanout")
+	if err != nil {
+		log.Fatalf("Unable to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewRecordFanoutServiceClient(conn)
+
+	fanoutFlags := flag.NewFlagSet("Fanout", flag.ExitOnError)
+	var id = fanoutFlags.Int("id", -1, "Id of the record to add")
+
+	if err := fanoutFlags.Parse(os.Args[2:]); err == nil {
+		res, err := client.Fanout(ctx, &pb.FanoutRequest{InstanceId: int32(*id)})
+		fmt.Printf("Fanout response: %v/%v", res, err)
+	}
+
+}
